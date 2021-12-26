bf3a114cee2260b62acc3af1c3293b9fb6a412ea
João Victor da Silva <jv782063@gmail.com>
2021-12-26 11:52:30 -0300

    sets the global style

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Routes from './routes';
 
+import './styles/sass/global.scss';
+
 const App: React.FC = () => {
   return <Routes />;
 };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import styles from './styles.module.scss';
+
 const Home: React.FC = () => {
   return <h1>Home</h1>;
 };
