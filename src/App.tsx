cf7e5016bf24ac8cea55750e4ab970991d92844a
João Victor da Silva <jv782063@gmail.com>
2021-12-26 11:03:23 -0300

    configure react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import Routes from './routes';
+
 const App: React.FC = () => {
-  return <h1>APP</h1>;
+  return <Routes />;
 };
 
 export default App;
---
+++ src/routes.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Routes as Switch, BrowserRouter } from 'react-router-dom';
+
+import Home from './pages/Home';
+
+const Routes: React.FC = () => (
+  <BrowserRouter>
+    <Switch>
+      ￼ <Route path="/" element={<Home />} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
